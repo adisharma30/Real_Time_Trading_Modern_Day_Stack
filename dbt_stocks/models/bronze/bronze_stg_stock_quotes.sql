e8141fc65f9f742ed4c06636486a788a113bebc8
Aditya Sharma <adisharma2104@gmail.com>
2026-09-20 18:00:35 +0200

    Initialize dbt project with stock data models, including bronze and silver layers, and add necessary configuration files

---
+++ dbt_stocks/models/bronze/bronze_stg_stock_quotes.sql
@@ -0,0 +1,12 @@
+SELECT
+v:c::float as current_price,
+v:d::float as change_amount,
+v:dp::float as change_percentage,
+v:h::float as day_high,
+v:l::float as day_low,
+v:o::float as day_open,
+v:pc::float as prev_close,
+v:t::timestamp as market_timestamp,
+v:symbol::string as symbol,
+v:fetched_at::timestamp as fetched_at
+from {{source('bronze', 'BRONZE_STOCK_QUOTE_RAW')}}
